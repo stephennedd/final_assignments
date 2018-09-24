0e74c2a663297dd651fee3c34b08374162b935b2
Stephen N <stephen.nedd@student.hu.nl>
2018-09-24 16:59:08 +0200

    first commit

--- final_assignment1.py
+++ final_assignment1.py
@@ -1,1 +1,11 @@
-print('Hello world')
+def standaardprijs(AfstandKM):
+    afstand = int(input('Enter the distance in KM to your destination: '))
+    prijs = afstand * 0.80
+    if afstand >= 50:
+        prijs = int(15) + (afstand*0.60)
+        print(prijs)
+    elif afstand <= int(0):
+        prijs = float(0.00)
+    print('The price of the ride is €',prijs)
+standaardprijs('a')
+
