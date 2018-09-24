5495174efaf058afd8c4040220c9ea1788e3a6d8
Stephen N <stephen.nedd@student.hu.nl>
2018-09-24 17:24:46 +0200

    Fixed decimal issue

--- final_assignment1.py
+++ final_assignment1.py
@@ -1,11 +1,14 @@
 def standaardprijs(AfstandKM):
-    afstand = int(input('Enter the distance in KM to your destination: '))
-    prijs = afstand * 0.80
+    afstand = float(input('Enter the distance in KM to your destination: '))
+    prijs = float(afstand * 0.80)
     if afstand >= 50:
         prijs = int(15) + (afstand*0.60)
         print(prijs)
     elif afstand <= int(0):
         prijs = float(0.00)
-    print('The price of the ride is €',prijs)
-standaardprijs('a')
+    print('The price of the journey is €',"%.2f" % round(prijs,2))
+standaardprijs(1)
 
+def ritprijs(leeftijd, weekendrit, afstandKM):
+    standaardprijs(1)
+    weekendrit = bool
