a2e703718659b56a34f3d61f6bcd50bbabfd16d7
Stephen N <stephen.nedd@student.hu.nl>
2018-09-25 16:42:15 +0200

    changed ritprijs

--- final_assignment1.py
+++ final_assignment1.py
@@ -1,14 +1,28 @@
-def standaardprijs(AfstandKM):
+def standaardprijs():
     afstand = float(input('Enter the distance in KM to your destination: '))
-    prijs = float(afstand * 0.80)
+    prijs = afstand * 0.80
     if afstand >= 50:
-        prijs = int(15) + (afstand*0.60)
+        prijs = 15 + (afstand*0.60)
         print(prijs)
-    elif afstand <= int(0):
+    elif afstand < int(0):
         prijs = float(0.00)
     print('The price of the journey is €',"%.2f" % round(prijs,2))
-standaardprijs(1)
 
-def ritprijs(leeftijd, weekendrit, afstandKM):
-    standaardprijs(1)
-    weekendrit = bool
+
+def ritprijs():
+    standaardprijs()
+    leeftijd = int(input('Please enter your age: '))
+    if leeftijd < 12:
+
+    weekendrit = str(input('Are you traveling on a weekend? True/False'))
+    if weekendrit == 'True':
+        weekendrit = True
+
+
+
+
+ritprijs()
+
+
+
+
